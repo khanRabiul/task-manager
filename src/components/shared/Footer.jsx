a13ce11d712b1c04451435860bf26838ed4d1018
khan-rabiul <krabiulislam2@gmail.com>
2024-10-06 22:03:46 +0600

    [HEADER & FOOTER] layout added

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,6 +1,13 @@
+
 const Footer = () => {
+    const fullDate = new Date();
+    const year = fullDate.getFullYear();
   return (
-    <div>Footer</div>
+    <footer className="fixed bottom-0 w-full bg-slate-600">
+        <div className="container mx-auto text-center py-3 ">
+            <a href="#" className=" capitalize text-sm">All rights received by task manager  &copy;{year}</a>
+        </div>
+    </footer>
   )
 }
 
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,10 +1,22 @@
 import { FcTodoList } from "react-icons/fc";
+import SearchTask from "../tasks/SearchTask";
 
 const Header = () => {
   return (
-    <div>Header 
-    <FcTodoList /></div>
-  )
-}
+    <header className="fixed top-0 w-full ">
+      <div className=" space-y-8 md:space-y-0  md:flex md:items-center md:justify-between px-8 py-8 shadow-lg ">
+        <div>
+          <a href="#" className="flex items-center space-x-4 text-4xl">
+            <FcTodoList />
+            <span>Task Manager</span>
+          </a>
+        </div>
+        <div>
+          <SearchTask />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
